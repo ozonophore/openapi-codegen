303ab1a118fb933c77458e87fe023302f7365a94
ALEXEY ZVEREV <Zverev.A.Nikol@sberbank.ru>
2021-09-05 22:32:29 +0300

    Add availability to set multi-configuration

--- bin/index.js
+++ bin/index.js
@@ -6,43 +6,55 @@ const path = require('path');
 const program = require('commander');
 const pkg = require('../package.json');
 
-const params = program
-    .name('openapi')
-    .usage('[options]')
-    .version(pkg.version)
-    .requiredOption('-i, --input <value>', 'OpenAPI specification, can be a path, url or string content (required)')
-    .requiredOption('-o, --output <value>', 'Output directory (required)')
-    .option('-c, --client <value>', 'HTTP client to generate [fetch, xhr, node]', 'fetch')
-    .option('--useOptions', 'Use options instead of arguments')
-    .option('--useUnionTypes', 'Use union types instead of enums')
-    .option('--exportCore <value>', 'Write core files to disk', true)
-    .option('--exportServices <value>', 'Write services to disk', true)
-    .option('--exportModels <value>', 'Write models to disk', true)
-    .option('--exportSchemas <value>', 'Write schemas to disk', false)
-    .option('--request <value>', 'Path to custom request file')
-    .parse(process.argv)
-    .opts();
-
 const OpenAPI = require(path.resolve(__dirname, '../dist/index.js'));
 
-if (OpenAPI) {
-    OpenAPI.generate({
-        input: params.input,
-        output: params.output,
-        httpClient: params.client,
-        useOptions: params.useOptions,
-        useUnionTypes: params.useUnionTypes,
-        exportCore: JSON.parse(params.exportCore) === true,
-        exportServices: JSON.parse(params.exportServices) === true,
-        exportModels: JSON.parse(params.exportModels) === true,
-        exportSchemas: JSON.parse(params.exportSchemas) === true,
-        request: params.request,
+function generate(config): void {
+    return OpenAPI.generate({
+        input: config.input,
+        output: config.output,
+        httpClient: config.client,
+        useOptions: config.useOptions,
+        useUnionTypes: config.useUnionTypes,
+        exportCore: JSON.parse(config.exportCore) === true,
+        exportServices: JSON.parse(config.exportServices) === true,
+        exportModels: JSON.parse(config.exportModels) === true,
+        exportSchemas: JSON.parse(config.exportSchemas) === true,
+        request: config.request,
     })
         .then(() => {
             process.exit(0);
         })
         .catch(error => {
-            console.error(error);
+            console.log(error);
             process.exit(1);
         });
+}
+
+if (OpenAPI) {
+    const fs = require('fs');
+    if (fs.existsSync('openapi.config.json')) {
+        const dataString = fs.readFileSync('openapi.config.json');
+        const configs = JSON.parse(dataString);
+        configs.map(config => {
+            generate(config);
+        });
+    } else {
+        const params = program
+            .name('openapi')
+            .usage('[options]')
+            .version(pkg.version)
+            .requiredOption('-i, --input <value>', 'OpenAPI specification, can be a path, url or string content (required)')
+            .requiredOption('-o, --output <value>', 'Output directory (required)')
+            .option('-c, --client <value>', 'HTTP client to generate [fetch, xhr, node]', 'fetch')
+            .option('--useOptions', 'Use options instead of arguments')
+            .option('--useUnionTypes', 'Use union types instead of enums')
+            .option('--exportCore <value>', 'Write core files to disk', true)
+            .option('--exportServices <value>', 'Write services to disk', true)
+            .option('--exportModels <value>', 'Write models to disk', true)
+            .option('--exportSchemas <value>', 'Write schemas to disk', false)
+            .option('--request <value>', 'Path to custom request file')
+            .parse(process.argv)
+            .opts();
+        generate(params);
+    }
 }
