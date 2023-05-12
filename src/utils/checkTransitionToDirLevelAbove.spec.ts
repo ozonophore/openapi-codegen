fea85152183dfdb91dd864bf8574b6af701a8f00
pidtchay <pidtchay@gmail.com>
2023-05-12 13:40:45 +0300

    #58 Fixed relative path calculation mechanism for model, model import and model schema.

--- src/utils/checkTransitionToDirLevelAbove.spec.ts
+++
@@ -1,8 +0,0 @@
-import { replaceTransitionToDirLevelAbove } from './checkTransitionToDirLevelAbove';
-
-describe('replaceTransitionToDirLevelAbove', () => {
-    it('should retur current path', () => {
-        const path = '../path1/path2/file';
-        expect(replaceTransitionToDirLevelAbove(path)).toEqual('path1/path2/file');
-    });
-});
--- src/utils/checkTransitionToDirLevelAbove.ts
+++
@@ -1,9 +0,0 @@
-/**
- * The function checks the transition to the directory with a level above and normalizes the path.
- * Input: "../../etc/abc"
- * Output: "etc/abc"
- */
-export function replaceTransitionToDirLevelAbove(path: string): string {
-    const anotherDirRegexp = /^(?:\.{2})?(?:\/\.{2})*\//;
-    return path.replace(anotherDirRegexp, '');
-}
