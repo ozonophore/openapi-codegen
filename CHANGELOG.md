fffb02dca50515df7f7817600b7d132cd109b934
pidtchay <pidtchay@gmail.com>
2025-01-24 12:30:10 +0300

    PDTCH-78 Fixed an error in generating models according to the specification when the schema referred to the schema in another file (#79)
    * PDTCH-78 Fixed an error in generating models according to the specification when the schema referred to the schema in another file and the schema from the file referred to the schema inside the same file
    
    * PDTCH-78 Added tests and did a little refactoring.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.5.1-rc.1] - 2025-01-21
+
+### Added
+- Added a test for the function getAbsolutePath
+- Added a test for the function getGatheringRefs
+
+### Fixed
+- Fixed an error in generating models according to the specification when the schema referred to the schema in another file. And the schema from the file referred to the schema inside the same file.
+
+### Updated
+- Moved the gatheringRefs function to a separate file
+
 ## [0.5.0-beta.0] - 2024-08-01
 
 ### Added
--- src/utils/getAbsolutePath.ts
+++ src/utils/getAbsolutePath.ts
@@ -1,16 +1,21 @@
 import { dirName, join } from '../core/path';
 
 export function getAbsolutePath(definitionRef: string | undefined, parentRef: string): string {
-    if (definitionRef) {
-        if (parentRef && parentRef.match(/^(#\/)/g)) {
-            return definitionRef;
-        } else if (definitionRef.match(/^(#\/)/g)) {
-            return `${parentRef}${definitionRef}`;
-        }
-        return join(dirName(parentRef), definitionRef);
+    // If the definition Ref is missing
+    if (!definitionRef) {
+        return parentRef.startsWith('#/') ? '' : parentRef;
     }
-    if (parentRef.match(/^(#\/)/g)) {
-        return '';
+
+    if (definitionRef.startsWith('#/')) {
+        // If definitionRef is an absolute reference (#/), replace the part after # in parentRef.
+        const basePath = parentRef.split('#')[0]; // We take everything up to #
+        return `${basePath}${definitionRef}`;
     }
-    return parentRef;
+
+    if (parentRef.startsWith('#/')) {
+        // If parentRef is an absolute reference, we return definitionRef as it is.
+        return definitionRef;
+    }
+
+    return join(dirName(parentRef), definitionRef);
 }
